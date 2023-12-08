8453278052e98542b1fdd8a8bb508b0553df794d
Joey Palchak <joey.palchak@gmail.com>
2023-12-08 14:50:00 -0800

    install axios

--- cranberry/src/components/SignIn.tsx
+++ cranberry/src/components/SignIn.tsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function SignIn() {
+
+  const [formData, setFormData] = useState<SignUpFormState>({
+    userName: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData(prevData => ({...prevData, [name]: value}));
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    fetch(`https://localhost:5001/api/users/register`)
+      .then(response => response.json())
+      .then((jsonResponse) => {
+        
+      })
+
+  }
+
   return (
     <section>
+      <h1>Register:</h1>
       <form className="register-form">
+      <input 
+          type="text" 
+          id="userName" 
+          name="userName" 
+          placeholder="User Name" 
+          required 
+        />
+        <br/>
         <input 
           type="text"
           id="email"
@@ -12,6 +44,7 @@ export default function SignIn() {
           placeholder="Email"
           required
         />
+        <br/>
         <input 
           type="password" 
           id="password" 
@@ -19,9 +52,16 @@ export default function SignIn() {
           placeholder="Password" 
           required 
         />
+        <br/>
         <button>Register</button>
       </form>
 
     </section>
   );
+}
+
+interface SignUpFormState {
+  userName: string;
+  email: string;
+  password: string;
 }
