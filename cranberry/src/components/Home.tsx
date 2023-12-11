38581d513df0f97f0f54af3649078cc8b11382ef
Joey Palchak <joey.palchak@gmail.com>
2023-12-11 14:16:18 -0800

    add placeholder splash page

--- cranberry/src/components/Home.tsx
+++ cranberry/src/components/Home.tsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <>
-      <h1>Landing Page</h1>
-    </>
+    <main style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <h1 style={headerStyle}>When cold turkey isn't enough</h1>
+
+      <Link to={"/register"}>
+        <button className="btn splash-btn">Sign Up</button>
+      </Link>
+    </main>
   );
+}
+
+const headerStyle = {
+  fontSize: "5em",
+}
+
+const buttonStyle = {
+  border: "2px red solid",
+  fontSize: "1.2em",
+  padding: "1em",
+  borderRadius: "3px",
+  background: "white",
+  color: "red",
 }
