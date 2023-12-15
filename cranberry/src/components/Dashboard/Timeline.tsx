082fcffab202e189e0376c0d353f6045e397ccdc
Joey Palchak <joey.palchak@gmail.com>
2023-12-15 14:37:16 -0800

    implement placeholder line chart for timeline component

--- cranberry/src/components/Dashboard/Timeline.tsx
+++ cranberry/src/components/Dashboard/Timeline.tsx
@@ -1,12 +1,65 @@
+import './Timeline.css';
+import { 
+  Chart as ChartJS,
+  LineElement, 
+  TimeScale, 
+  LinearScale, 
+  PointElement, 
+  Tooltip, 
+  Legend
+} from 'chart.js';
+import 'chartjs-adapter-date-fns';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  LineElement, 
+  TimeScale, 
+  LinearScale, 
+  PointElement, 
+  Tooltip, 
+  Legend
+);
 
 export default function Timeline() {
+
+  const data = {
+    labels: ['2023-12-11T11:58', '2023-12-10T11:58', '2023-12-09T11:58'], // dates
+    datasets: [
+      {
+        label: 'Cravings',
+        data: [8, 2, 6], // smoked
+        backgroundColor: 'blue',
+        borderColor: 'black',
+        tension: 0.4
+      }
+    ]
+  };
+
+  const options = {
+    scales: {
+      x: {
+        type: 'time',
+        time: {
+          unit: 'day'
+        }
+      },
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
   return  (
     <section className="user-timeline dash-section">
-      <h1>Timeline</h1>
+      <div className="section-header">
+        <h1>Timeline</h1>
+      </div>
+
       <div className="timeline-content">
-        <h1>Data Visualization Here</h1>
-        <div>
-          <canvas id="userChart"></canvas>
+        <div className="time-scale">
+          <Line
+            data={data}
+            options={options as any}
+          ></Line>
         </div>
       </div>
     </section>
