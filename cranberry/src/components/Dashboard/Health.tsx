17e04a32c1a66c7bb7f8b01cb8e757cd4a89f368
Joey Palchak <joey.palchak@gmail.com>
2023-12-16 00:01:09 -0800

    calculate user progress for each health milestone

--- cranberry/src/components/Dashboard/Health.tsx
+++ cranberry/src/components/Dashboard/Health.tsx
@@ -1,25 +1,41 @@
 import './Health.css';
 import { LinearProgress } from '@mui/material';
 import { healthBenefitsOverTime } from '../../data/health-benefits';
-import intervalToDuration from 'date-fns/intervalToDuration';
+import differenceInHours from 'date-fns/differenceInHours';
 import type { IUser } from '../../types';
 
 export default function Health({ user }: { user: IUser }) {
 
-  const elapsedTime = intervalToDuration({ start: new Date(user.quitDate), end: new Date() })
+  // To calculate progress towards each health benefit,
+  // we calculate the difference in hours between now and the user's quit date,
+  // and compare with the number of hours needed to reach a benefit
 
+  const elapsedTime = differenceInHours(new Date(), new Date(user.quitDate));
+
+  const healthProgress = (elapsedHours: number, targetHours: number) => {
+    const progress = (elapsedHours / targetHours) * 100;
+    return progress > 100 ? 100 : progress;
+  };
+
+  // TODO: refactor into smaller components
 
   return  (
     <section className="user-health dash-section">
       <h1>Health Progress</h1>
       <div className="health-content">
         <div className="health-progress">
+
           {healthBenefitsOverTime.map((item, index) => (
             <div className="health-item" key={index} >
               <h4>{item.benefit}</h4>
-              <LinearProgress className="progress-bar" variant='determinate' value={30} />
+              <LinearProgress 
+                className="progress-bar" 
+                variant='determinate' 
+                value={healthProgress(elapsedTime, item.time)} 
+              />
             </div>
           ))}
+
         </div>
 
         <div className="references">
