d93c417ec677fe76eb640bbca909ff71e6e09264
Joey Palchak <joey.palchak@gmail.com>
2023-12-15 18:45:20 -0800

    refactor calcElapsedTime into own util function

--- cranberry/src/hooks/useElapsedTime.ts
+++ cranberry/src/hooks/useElapsedTime.ts
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import calculateElapsedTime from '../util/calcElapsedTime';
 
 export default function useElapsedTime(quitDate: Date) {
   
@@ -13,29 +14,11 @@ export default function useElapsedTime(quitDate: Date) {
 
     const timeInterval = setInterval(() => {
       setElapsedTime(calculateElapsedTime(quitDate));
-      }, 1000 // if a day has passed, count by minutes, otherwise count each second
+      }, 1000 
     );
-      console.log("tick")
+      console.log("tick") // ! CONSOLE LOG
     return () => clearInterval(timeInterval);
   }, [quitDate])
-
-  const calculateElapsedTime = (startDate: Date | string): ElapsedTime => {
-
-    startDate = (typeof startDate === 'string' ? new Date(startDate) : startDate);
-
-    const currentTime = new Date();
-
-    // get difference in seconds between now and given date
-    const difference = Math.floor((currentTime.getTime() - startDate.getTime()) / 1000);
-    // calculate the time that has passed in days / hours / minutes / seconds
-    const days = Math.floor(difference / (24 * 60 * 60));
-    const hours = Math.floor((difference % (24 * 60 * 60)) / (60 * 60));
-    const minutes = Math.floor((difference % (60 * 60)) / 60);
-    const seconds = Math.floor(difference % 60);
-
-    return { days, hours, minutes, seconds };
-  };
-
 
   return elapsedTime;
 }
