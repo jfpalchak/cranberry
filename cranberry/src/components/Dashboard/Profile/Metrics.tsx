2c1ba9aa0c7884326719a76b40c843c0f7166096
Joey Palchak <joey.palchak@gmail.com>
2023-12-21 12:33:18 -0800

    add tooltips for each metric tracker

--- cranberry/src/components/Dashboard/Profile/Metrics.tsx
+++ cranberry/src/components/Dashboard/Profile/Metrics.tsx
@@ -3,10 +3,15 @@ import SmokeFreeIcon from '@mui/icons-material/SmokeFree';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import MoreTimeIcon from '@mui/icons-material/MoreTime';
 import RestoreIcon from '@mui/icons-material/Restore';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import { 
+  moneyText, 
+  avoidedText, 
+  lifeText, 
+  timeText } from "../../../data/metrics-text";
 import { Tooltip } from "@mui/material";
 import type { IUser } from "../../../types";
 
-const moneyTrackerText = "This is calculated by multiplying the number of cigarettes you would have otherwise smoked by the cost of one cigarette per pack, according to your registration data.";
 
 function Metrics({ user }: { user: IUser }) {
 
@@ -17,15 +22,16 @@ function Metrics({ user }: { user: IUser }) {
   return (
     <div className="progress-trackers">
 
-      <Tooltip title={moneyTrackerText} placement="top-start" >
       <div className="money-tracker tracker">
-        <AttachMoneyIcon />
+        <AttachMoneyIcon className="tracker-icon" />
         <div className="info">
           <h3>{userProgress?.moneySaved.toFixed(2)}</h3>
           <p>money saved</p>
         </div>
+        <Tooltip title={moneyText} placement="top-start" >
+          <HelpOutlineIcon id="money-question" />
+        </Tooltip>
       </div>
-      </Tooltip>
 
       <div className="avoided-tracker tracker">
         <SmokeFreeIcon />
@@ -33,6 +39,9 @@ function Metrics({ user }: { user: IUser }) {
           <h3>{userProgress?.cigsAvoided.toFixed()}</h3>
           <p>cigarettes avoided</p>
         </div>
+        <Tooltip title={avoidedText} placement="top-start" >
+          <HelpOutlineIcon id="avoided-question" />
+        </Tooltip>
       </div>
 
       <div className="time-gained-tracker tracker">
@@ -41,6 +50,9 @@ function Metrics({ user }: { user: IUser }) {
           <h3>{(userProgress?.timeSaved! / 60).toFixed(2)}</h3>
           <p>time saved (hours)</p>
         </div>
+        <Tooltip title={timeText} placement="top-start" >
+          <HelpOutlineIcon id="time-question" />
+        </Tooltip>
       </div>
 
       <div className="life-regained-tracker tracker">
@@ -49,6 +61,9 @@ function Metrics({ user }: { user: IUser }) {
           <h3>{(userProgress?.timeGained! / 24).toFixed(2)}</h3>
           <p>time gained (days)</p>
         </div>
+        <Tooltip title={lifeText} placement="top-start" >
+          <HelpOutlineIcon id="life-question" />
+        </Tooltip>
       </div>
 
     </div>
---
+++ cranberry/src/data/metrics-text.ts
@@ -0,0 +1,4 @@
+export const moneyText = "This is calculated by multiplying the number of cigarettes you would have otherwise smoked by the cost of one cigarette per pack, according to your registration data.";
+export const avoidedText = "According to your registered data, multiply the average number of cigarettes you might have had in a day by the number of days that have passed.";
+export const timeText = "This is calculated by multiplying the number of cigarette's you've not smoked by (an average time of) five minutes.";
+export const lifeText = "On average, a smoker who quits by the age of 35 lives 10 years longer than those who continue to smoke. If 75 is the average lifespan, then someone who quits at 35 regains about a quarter of their life, or, about 6 hours for every 24 hours not smoked.";
