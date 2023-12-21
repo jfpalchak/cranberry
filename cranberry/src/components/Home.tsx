b0af44f9289910b10d09c71ac76ab6cdd8756572
Joey Palchak <joey.palchak@gmail.com>
2023-12-20 19:28:20 -0800

    add cloud loop to hero page

--- cranberry/src/components/Home.tsx
+++ cranberry/src/components/Home.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
-import cloud from './../img/cloud.png';
+import cloud from './../img/cloudRight.png';
+import cloud2 from './../img/cloudLeft.png';
+
 
 export default function Home() {
 
@@ -7,6 +9,7 @@ export default function Home() {
     <main className="hero">
       <div className="hero-call">
         <img className="hero-cloud" src={cloud} alt="" />
+        <img className="hero-cloud-2" src={cloud2} alt="" />
         <div className="hero-text">
           <h1>When cold turkey isn't enough.</h1>
         </div>
