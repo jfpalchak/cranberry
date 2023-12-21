5c3e740d530cfefa0abb197597400da849d1cad3
Joey Palchak <joey.palchak@gmail.com>
2023-12-20 17:10:56 -0800

    add placeholder logo img to header

--- cranberry/src/components/Header.tsx
+++ cranberry/src/components/Header.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import berry from './../img/berry1.png';
+import berry2 from './../img/berry2.jpg';
+import can from './../img/canberries.png';
 
 export default function Header({ loggedIn }: HeaderProps) {
 
@@ -7,6 +10,7 @@ export default function Header({ loggedIn }: HeaderProps) {
     <header>
       <div className="logo">
         <Link to={!loggedIn ? "/" : "/dashboard/profile"}>
+          <img src={berry} alt="Cranberry Logo" />
           <span style={ { color: "rgb(240, 88, 88)" } }>Cranberry</span>
         </Link>
       </div>
