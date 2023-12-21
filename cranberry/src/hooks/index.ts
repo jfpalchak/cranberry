3ff3d9fccd69e8941001e4a4ae4c3daba91cae78
Joey Palchak <joey.palchak@gmail.com>
2023-12-20 23:37:31 -0800

    create branch for readme images

--- cranberry/src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as useProgressCalculations } from './useDataCalculations';
-export { default as useElapsedTime } from './useElapsedTime';
-export { default as useHealthBenefits } from './useHealthBenefits';
--- cranberry/src/store/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
-
-// Use throughout app instead of vanilla `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
